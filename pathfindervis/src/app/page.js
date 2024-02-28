44d55c001e00a02a0f28a4c6d2ac8a32956782be
Jabir Mustahid <jabirm2002@gmail.com>
2024-02-27 19:00:16 -0500

    Update page.js

--- pathfindervis/src/app/page.js
+++ pathfindervis/src/app/page.js
@@ -1,28 +1,68 @@
 "use client";
-import { Line } from 'react-chartjs-2';
+import { Bar } from 'react-chartjs-2';
+import Chart from 'chart.js/auto';
 import { useState } from 'react';
-export default function Home() {
-  let array = []
 
-  for (let i = 0; i <= 10; i++) {
-    array.push(Math.floor(Math.random() * 100))
-  }
-  console.log(array)
-  
-  const [newarray, setNewarray] = useState({
-    labels: array,
+
+const chartOptions = {
+  scales: {
+    y: {
+      max: 100,
+      beginAtZero: true,
+    },
+  },
+};
+
+export default function Home() {
+
+  const [newarray, setNewarray] = useState([0,0,0,0,0,0,0,0,0,0,0])
+  const [newarraydataset, setNewarraydataset] = useState({
+    labels: newarray,
     datasets: [
       {
-        label: "Sort",
-        data: array
+        data: newarray
       }
     ]
   });
+  
+  const generateNewArray = (e) => {
+    let randomlist = []
+    for (let i = 0; i <= 10; i++) {
+      randomlist.push(Math.floor(Math.random() * 100))
+    }
+
+    setNewarray((p) => {
+      setNewarraydataset({
+        labels: randomlist,
+        datasets: [  
+          {
+            data: randomlist,
+          },
+        ],
+  
+      });
+      return randomlist;   
+    });
+
+
+    console.log(newarray)
+  }
+  
+  
+  const selectionSort = () => {
+    console.log(newarray)
+  };
+
 
   return (
     <div>
-      <p>Hello world</p>
-      <Line data={newarray}/>
+      <navbar>
+        <button onClick={generateNewArray} className='bg-blue-100'>Generate new array</button>
+        <button onClick={selectionSort} className='mx-2 bg-blue-100'>Sort Arrary</button>
+      </navbar>
+      <div className='mx-20 p-20'>
+        <Bar data={newarraydataset} options={chartOptions}/>
+      </div>
     </div>
   );
 }
